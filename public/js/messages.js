d49ed19be580b68f449f3daeb5cd57931b0484e0
tonybergeron <tonybergeron@gmail.com>
2013-09-20 10:52:31 -0500

    adding autoscroll to messages incoming

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,23 +1,32 @@
-var urlRegex= "";
-  function message (from, msg) {
-    var message = msg.replace(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/gi, "<a href='$&' target='_blank'>$&</a>");
-    
-    $('#lines').append($('<p>').append($('<b>').text(from), message));
+var urlRegex = "";
+
+function message(from, msg) {
+  var message = msg.replace(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/gi, "<a href='$&' target='_blank'>$&</a>");
+
+  $('#lines').append($('<p>').append($('<b>').text(from), message))
+    .animate({
+      "scrollTop": $('#lines')[0].scrollHeight
+    }, "fast");
 
 
-    //Truncate for the Alert
-    /*var alertMsg = from + ": " + msg;
+  //Scroll to the bottom automatically on new message
+  //$("#lines").animate({ scrollTop: $('#lines').height()}, 1000);
+  //$('#lines').scrollTop($('#lines').height())
+
+
+  //Truncate for the Alert
+  /*var alertMsg = from + ": " + msg;
     if(alertMsg.length < 30) {
 
     } else {
     	alertMsg = alertMsg.substring(0,27) + "...";
     }
     */
-   	$.titleAlert("Message from " + from, {
-	    requireBlur:false,
-	    stopOnFocus:true,
-	    stopOnMouseMove:true,
-	    duration:0,
-	    interval:300
-	});
-  }
+  $.titleAlert("Message from " + from, {
+    requireBlur: false,
+    stopOnFocus: true,
+    stopOnMouseMove: true,
+    duration: 0,
+    interval: 1000
+  });
+}
